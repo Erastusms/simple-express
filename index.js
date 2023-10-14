1cd885f07a404fac58608fe618822284d07c5431
Erastus Mauliate <emauliate17@gmail.com>
2023-10-14 20:26:11 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Add Express
 const express = require('express');
-
+const mongoose = require('mongoose');
 // Initialize Express
 const app = express();
+const PORT = 5555;
+
+const mongoDBURL =
+  'mongodb://admin:admin@ac-2jcfvlj-shard-00-00.nemoveh.mongodb.net:27017,ac-2jcfvlj-shard-00-01.nemoveh.mongodb.net:27017,ac-2jcfvlj-shard-00-02.nemoveh.mongodb.net:27017/books-collection?ssl=true&replicaSet=atlas-2bzsn4-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+
 
 // Create GET request
 app.get('/', (req, res) => {
@@ -25,9 +33,21 @@ app.get('/api/:id', async (request, response) => {
 });
 
 // Initialize server
-app.listen(5000, () => {
-  console.log('Running on port 5000.');
-});
+// app.listen(5000, () => {
+//   console.log('Running on port 5000.');
+// });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Export the Express API
 module.exports = app;
